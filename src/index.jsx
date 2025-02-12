46a98f9e0c4129453ca8ac69473105ecd4785d40
balajidamodaran75 <48764827+balajidamodaran75@users.noreply.github.com>
2025-02-12 18:34:41 -0500

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Fun(){
+  return (
+  <div>
+    <h1>React is really fun!</h1>
+    <img src="logo192.png" alt="logo"/>
+  </div>
+  )
+}
+  
+
+root.render(
+  <div>
+    <Fun />
+  </div>
+);
 
